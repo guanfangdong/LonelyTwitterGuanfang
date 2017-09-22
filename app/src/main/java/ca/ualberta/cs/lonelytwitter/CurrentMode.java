71d068bc349f28fb4c5b4de95015113af23c8996
guanfangdong <dong.guanfang.2014@gmail.com>
2017-09-21 20:58:59 -0600

    add_clear

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/CurrentMode.java
@@ -0,0 +1,9 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by guanfang on 2017/9/14.
+ */
+
+public abstract class CurrentMode{
+    public abstract String getCurrentMood();
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/MoodSad.java
@@ -0,0 +1,11 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by guanfang on 2017/9/14.
+ */
+
+public class MoodSad extends  CurrentMode {
+    public String getCurrentMood(){
+        return ("sad");
+    }
+}
